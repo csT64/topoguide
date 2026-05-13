64bbd3b78bc5447d02c1c7eb7ba16099ebb1ab3a
csT64 <c.segura@tourisme64.com>
2026-05-13 13:39:25 +0200

    Ajustements manuels header/footer PDF

--- components/pdf/TopoguideService.php
+++ components/pdf/TopoguideService.php
@@ -116,25 +116,120 @@ class TopoguideService
         $adresse = implode(' &mdash; ', $parts);
 
         $bgCss = file_exists($imgPath)
-            ? 'background-image:url("file://' . $imgPath . '");background-repeat:repeat-x;background-size:auto 100%;'
+            ? 'background-image:url("file://' . $imgPath . '");background-repeat: repeat-x;background-size:100%;'
             : '';
 
-        return '<html height="30cm" style="margin:0;padding:0;"><head><meta charset="UTF-8"><style>'
+        return '<html style="margin:0;padding:0;"><head><meta charset="UTF-8"><style>'
             . '*{margin:0;padding:0;}'
-            . 'html,body{width:100%;height:30cm;}'
+            . 'html,body{width:100%;}'
             . 'body{' . $bgCss . '}'
-            . 'table{width:100%;height:30cm;border-collapse:collapse;}'
-            . 'td{color:#fff;font-family:Arial,sans-serif;font-size:9pt;text-align:center;'
+            . 'table{width:100%;height:2.5cm;border-collapse:collapse;}'
+            . 'td{color:#fff;font-family:Arial,sans-serif;font-size:11pt;text-align:center;'
             . 'vertical-align:middle;padding:0 9mm;}'
             . '</style></head>'
-            . '<body height="30cm" style="height:30cm;margin:0;padding:0;">'
-            . '<table><tr><td>' . $adresse . '</td></tr></table>'
+            . '<body style="margin:0;padding:0;">'
+            . '<table border="1" style="border:1px solid red;"><tr><td>' . $adresse . '</td></tr></table>'
             . '</body></html>';
     }
 
+    
     private function buildHeaderHtml(string $pix): string
+{
+    $height  = '80px';
+    $imgPath = $pix . '/haut_page.png';
+
+    // Picto difficulté
+    $difficulte = $this->iti->getDifficulteVal();
+
+    $diffMap = [
+        'Très facile' => 1,
+        'Very easy'   => 1,
+        'Muy fácil'   => 1,
+
+        'Facile'      => 2,
+        'Easy'        => 2,
+        'Fácil'       => 2,
+
+        'Moyenne'     => 3,
+        'Average'     => 3,
+        'Medio'       => 3,
+    ];
+
+    $diffNiv  = $diffMap[$difficulte] ?? 4;
+    $diffPath = $pix . '/picto-niv-' . $diffNiv . '.png';
+
+    // Fond header
+    $bgImg = '';
+    if (file_exists($imgPath)) {
+        $bgImg = '<img 
+            class="bg"
+            src="file://' . $imgPath . '"
+            alt="">';
+    }
+
+    // Picto difficulté
+    $diffImg = '';
+    if (file_exists($diffPath)) {
+        $diffImg = '<img 
+            class="diff"
+            src="file://' . $diffPath . '"
+            alt="">';
+    }
+
+    return '
+<html>
+<head>
+<meta charset="UTF-8">
+
+<style>
+html,
+body{
+    margin:0;
+    padding:0;
+    width:210mm;
+    height:' . $height . ';
+}
+
+body{
+    position:relative;
+    height:180px;
+}
+
+/* image de fond */
+.bg{
+    position:relative;
+
+    width:1480px;
+display:block;
+margin:0;
+padding:0;
+}
+
+/* picto difficulté */
+.diff{
+    position:absolute;
+
+    top:40%;
+    left:160%;
+;
+}
+</style>
+
+</head>
+
+<body>
+
+' . $bgImg . '
+
+' . $diffImg . '
+
+</body>
+</html>';
+}
+    
+    private function buildHeaderHtmlold(string $pix): string
     {
-        $height  = '25mm';
+        $height  = '20mm';
         $imgPath = $pix . '/haut_page.png';
 
         // Picto difficulté — même logique que fiche.php
@@ -146,19 +241,22 @@ class TopoguideService
         $diffPath   = $pix . '/picto-niv-' . $diffNiv . '.png';
 
         $bgCss = file_exists($imgPath)
-            ? 'background-image:url("file://' . $imgPath . '");background-repeat:repeat-x;background-size:auto 100%;'
+            ? 'display:block;overflow:hidden;background-size:100%;background-image:url("file://' . $imgPath . '");'
             : '';
+        
+      $diffImg =  '<img src="file://' . $imgPath . '" style="">';
+        
 
         $diffImg = file_exists($diffPath)
             ? '<img src="file://' . $diffPath . '" style="position:absolute;top:5mm;right:5mm;height:15mm;" alt="">'
             : '';
 
-        return '<html height="30cm" style="margin:0;padding:0;"><head><meta charset="UTF-8"><style>'
+        return '<html style="margin:0;padding:0;"><head><meta charset="UTF-8"><style>'
             . '*{margin:0;padding:0;}'
-            . 'html,body{width:100%;height:30cm;position:relative;}'
+            . 'html,body{width:100%;border:1px solid blue;}'
             . 'body{' . $bgCss . '}'
             . '</style></head>'
-            . '<body height="30cm" style="height:30cm;margin:0;padding:0;">'
+            . '<body style="margin:0;padding:0;" >'
             . $diffImg
             . '</body></html>';
     }
